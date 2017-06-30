d0a2529fe9d55430fd6e9b951e06a61e27300a6c
Admin <admin@Admins-MacBook-Pro.local>
2017-06-29 23:44:49 -0400

    second commit

--- scdata/src/components/Hello.vue
+++ scdata/src/components/Hello.vue
@@ -2,6 +2,157 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
+    
+<svg version="1.1"
+	 id="svg7197" sodipodi:version="0.32" sodipodi:docname="South Carolina Republican Presidential Primary Election Results by County, 2012.svg" inkscape:version="0.48.1 " inkscape:output_extension="org.inkscape.output.svg.inkscape" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+	 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="304.1px" height="231.2px"
+	 viewBox="-153 281.8 304.1 231.2" enable-background="new -153 281.8 304.1 231.2" xml:space="preserve">
+<defs>
+	
+</defs>
+<sodipodi:namedview  inkscape:pageopacity="0.0" inkscape:pageshadow="2" inkscape:document-units="px" inkscape:current-layer="layer1" inkscape:window-width="1024" inkscape:window-height="721" inkscape:window-x="-4" inkscape:window-y="-4" inkscape:window-maximized="0" pagecolor="#ffffff" bordercolor="#666666" borderopacity="1.0" gridtolerance="10000" guidetolerance="10" objecttolerance="10" inkscape:zoom="1.3676923" inkscape:cx="173.36699" inkscape:cy="134.06647" showgrid="false" id="base">
+	</sodipodi:namedview>
+<g id="layer1" transform="translate(-349.15674,-349.78272)" inkscape:label="Layer 1" inkscape:groupmode="layer">
+	<path id="SC_Pickens" inkscape:label="Pickens, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M232.8,656.8
+		l11.1-1.3l2.6,3.2l4.2,9.2l0.6,1.7l0.5,2.5l-0.3,0.4l-18.7,17.1l-5.8-15.8l-3.9-9.7l1-3.7l1-1.6L232.8,656.8"/>
+	<path id="SC_Oconee" inkscape:label="Oconee, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M225.2,658.8l-1,1.6
+		l-1,3.7l3.9,9.7l5.8,15.8l-7.5,11.6l-3.7-0.5l-3.6-2.5l-15-9.2l-1.2-1.9l1-5.5l3.9-7.1l5.4-4.2l1.3-5.8l5.6-2.7L225.2,658.8"/>
+	<path id="SC_Beaufort" inkscape:label="Beaufort, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M373.1,809.7
+		l23.5,12.1l3.5,5.1l0.3,0.6l0.2,0.9l-0.1,1.1l-0.7,3.7l-0.6,0.6l-15,15.6l-3.5,2.8l-3.6,2.7l-1.5-1.2l-7.3-5.8l-1-1.5l-0.5-1.6
+		l0.1-0.4l0.8-3.2l3.2-2.8l3.2-3.4l1.7-2.2l0.4-2l0-4.7l-2.3-8.6l-0.3-0.7l-1.1-1.6l-1.6-2L373.1,809.7"/>
+	<path id="SC_Hampton" inkscape:label="Hampton, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M373.1,809.7
+		l-2.1,3.5l-1.5,3.1l-1.1-0.3l-1.8-0.7l-0.5-1.4l0.2-1l-0.2-1l-2.2-2.1l-2.5-1.6l-1.2,1.1l-13.3,15.9l-6.3-2.5l-2.6-10.6l1.1-0.6
+		l2.6-1.8l2.5-4.3l0.5-3.4l0.2-2.7l1-2.9l0.8-1.1l0.7-0.6l6.7-5.9L373.1,809.7"/>
+	<path id="SC_Jasper" inkscape:label="Jasper, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M371,813.2l1.6,2
+		l1.1,1.6l0.3,0.7l2.3,8.6l0,4.7l-0.4,2l-1.7,2.2l-3.2,3.4l-3.2,2.8l-0.8,3.2l-0.1,0.4l0.5,1.6l1,1.5l7.3,5.8l1.5,1.2l0,3.4
+		l-11.3-2.1l-3.4-1.3l-0.3-0.3l-0.8-3.1l-0.6-2.9l-0.3-1.4l-0.8-3.8l-6.4-13l-5.4-4.5l-1.2-0.4l13.3-15.9l1.2-1.1l2.5,1.6l2.2,2.1
+		l0.2,1l-0.2,1l0.5,1.4l1.8,0.7l1.1,0.3L371,813.2"/>
+	<path id="SC_Marion" inkscape:label="Marion, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M430.6,687.5l0.7-1.6
+		l3.3-3.4l3.1-1.4l1.8-0.4l12.3-1.2l3.1,1.9l-4.3,13.8l-1.7,3l-2.5,3.5l-0.2,0.5l0.3,2.2l0.2,0.8l6.1,9.1l3.2,4.4l2.3,2.7l-8.4-4
+		l-0.7-1.3l-11.2-8.4l-0.6-0.6l-1-1.8l-6.4-13.7l-0.3-0.5v-0.3L430.6,687.5"/>
+	<path id="SC_Dillon" inkscape:label="Dillon, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M431.3,658.4
+		l26.3,19.1l-2.8,3.9l-3.1-1.9l-12.3,1.2l-1.8,0.4l-3.1,1.4l-3.3,3.4l-0.7,1.6l-5.9-4L431.3,658.4"/>
+	<path id="SC_Horry" inkscape:label="Horry, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M454.8,681.4l2.8-3.9
+		l29.3,20.9l7.7,5.5l-2.5,1l-4.8,2.9l-2.3,1.7l-3,2.9l-0.3,0.3l-1.2,1.3l-0.6,0.6l-0.9,1.2l-1.2,1.6l-0.4,0.5l-3.6,5.5l-2.1,4.1
+		l-0.8,1.5l-12.8-7.6l-2.3-2.7l-3.2-4.4l-6.1-9.1l-0.2-0.8l-0.3-2.2l0.2-0.5l2.5-3.5l1.7-3L454.8,681.4"/>
+	
+		<path id="SC_Charleston" inkscape:label="Charleston, SC" fill="#C60E3B" stroke="#FFFFFF" stroke-width="1.157" stroke-miterlimit="10" d="
+		M449.3,759.3l10.5,5.8l-2.8,8.1l-2.6,0.7l-4.7,1l-6.4,6.7l-4.3,6.3l-8.9,10.1l-10.3,11.3l-0.6,0.3l-0.5,0.3l-2,1.1l-4.6,3.3
+		l-2.4,2.3l0,0l-0.5-0.4l-3.7,1.6l-0.7,0.8l0.7,0.6l0.1,0.2l-0.1,0.5l-0.5,0.1l-1.1-0.3l-2.6-3.6l-3.6-7.9l-2.2-3.9l1.3-6.4
+		l-0.2-3.5l7.1-0.3l7.9,0.7l0.6-0.1l2.3-5.3l0.6-3.2l-5.3-6.1l7.2,3.6l6.2,2.6l2,5.5l0.9,0.4l0.6-0.2l0.5-0.4l6.4-10.6l1.6-5.2
+		l3.5-7.5l8.6-7.9L449.3,759.3"/>
+	<path id="SC_Dorchester" inkscape:label="Dorchester, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M394.3,765.5
+		l1,0.7l3.8,4.3l-0.6,1.5l-0.6,0.7l2,1.3l9.9,6.3l5.3,6.1l-0.6,3.2l-2.3,5.3l-0.6,0.1l-7.9-0.7l-7.1,0.3l-1.3-12.7l-0.3-0.7
+		l-0.7-0.5h-0.2l-7.3,0.6l-4.1,0.9l-1.9-0.3l-0.3-0.2l-11-6.5l15.3-13.8l0.3,0.3l0.3,0.4l0.1,0.6l0,0.8l0.2,0.5l0.7,1.1l0.7,0.4
+		l3.1,0.7L394.3,765.5"/>
+	<path id="SC_Berkeley" inkscape:label="Berkeley, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M406.9,745.7
+		l1.1-0.9l0.4-0.1l4.8-0.6l1.7,0.2l4.4,2.6l2,1.5l0.9,1.3l0.4,0.4l6.3,3.4l5.5,1.8l14.8,4.2l-2.1,1.1l-8.6,7.9l-3.5,7.5l-1.6,5.2
+		l-6.4,10.6l-0.5,0.4l-0.6,0.2l-0.9-0.4l-2-5.5l-6.2-2.6l-7.2-3.6L400,774l-2-1.3l0.6-0.7l0.6-1.5l-3.8-4.3l-1-0.7l6.1-14.8
+		L406.9,745.7"/>
+	<path id="SC_Georgetown" inkscape:label="Georgetown, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.157" d="M449.8,717.4
+		l8.4,4l12.8,7.6l-3.7,7.2l-0.2,0.5l-0.4,1.1l-0.1,0.3l0,0.1l0,0.1l-1.2,4.3l-0.2,0.7l-0.2,1l-0.1,0.2l-0.2,3.8l-0.1,2l-0.1,3.3
+		l0,0.1l0,0l0.2,0.9l0,0.2l0.9,2.2l-0.5,2.5l-0.4,0.6l-2.5,3.5l-2.5,1.5l-10.5-5.8l-14.8-4.2l-0.4-1l0-1.3l3.8-8.5l6.8-12.5l0.7-6.9
+		l0.8-2l2.4-5l0.3-0.3L449.8,717.4"/>
+	<path id="SC_Clarendon" inkscape:label="Clarendon, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M408.9,712.2
+		l6.6,3.4l-8.6,30.1l-6.5,5l-5.9,1.9l-1.7,0.3l-0.8-0.1l-1.9-0.6l-2.4-1.6l-1.9-1.8l-3.1-3.7l-3.3-5.7l6.6-10.3l11.1-7.1
+		L408.9,712.2"/>
+	<path id="SC_Williamsburg" inkscape:label="Williamsburg, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="
+		M449.1,716.1l0.7,1.3l-0.9,0.3l-0.3,0.3l-2.4,5l-0.8,2l-0.7,6.9l-6.8,12.5l-3.8,8.5l0,1.3l0.4,1l-5.5-1.8l-6.3-3.4l-0.4-0.4
+		l-0.9-1.3l-2-1.5l-4.4-2.6l-1.7-0.2l-4.8,0.6l-0.4,0.1l-1.1,0.9l8.6-30.1l5.2,2.8l4.6,0.8l14,0.3l3.1-1.1l0.5-0.8l2.8-2.2l0.4-0.2
+		l1.2,0.1L449.1,716.1"/>
+	<path id="SC_Florence" inkscape:label="Florence, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M423.1,683.2
+		l1.5,0.3l5.9,4l-0.9,3.3v0.3l0.3,0.5l6.4,13.7l1,1.8l0.6,0.6l11.2,8.4l-1.7-0.8l-1.2-0.1l-0.4,0.2l-2.8,2.2l-0.5,0.8l-3.1,1.1
+		l-14-0.3l-4.6-0.8l-5.2-2.8l-6.6-3.4l-2.8-7l-4.7-2l12-12.9l5.6-5.5L423.1,683.2"/>
+	<path id="SC_Marlboro" inkscape:label="Marlboro, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M401.2,649.9
+		l14-2.3l15.4,10.2l0.8,0.6l-6.7,25l-1.5-0.3v-0.3l-1.8-4.5l-0.9-1.8l-2.2-0.9l-0.4,0l-0.2,0.1l-0.2-0.1l-7.1-5.4l-0.1-0.2l-0.2-1.2
+		l0.1-0.4L401.2,649.9"/>
+	<path id="SC_Darlington" inkscape:label="Darlington, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M410.3,668.7
+		l-0.1,0.4l0.2,1.2l0.1,0.2l7.1,5.4l0.2,0.1l0.2-0.1l0.4,0l2.2,0.9l0.9,1.8l1.8,4.5v0.3l-4.1,1.4l-5.6,5.5l-12,12.9l-0.2,0.1
+		l-4.9-3.4l-0.9-1.2l1.6-2.2l0.3-3.1l-2.4-3.9l-2.5-2.5l-2-0.6l-5.1-1v-0.2l12.7-11.5L410.3,668.7"/>
+	<path id="SC_Lee" inkscape:label="Lee, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M385.4,685.3l5.1,1l2,0.6
+		l2.5,2.5l2.4,3.9l-0.3,3.1l-1.6,2.2l0.9,1.2l4.9,3.4l0.2-0.1l4.7,2l-1.1,3.1l-7.5,5.1l-0.9,0.5l-19.2-8.5l-1-4.2l0.8-0.9l1.4-3.8
+		l-0.3-2.2l-0.2-0.9l1.7-2.3L385.4,685.3"/>
+	<path id="SC_Kershaw" inkscape:label="Kershaw, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M375.4,668.4
+		l10,16.7v0.2l-5.5,5.8l-1.7,2.3l0.2,0.9l0.3,2.2l-1.4,3.8l-0.8,0.9l-7.2,6.3l-5.7,3.2l-10.2-6.7l1.6-6.6l-5.3-13.1l7-7.2l0.3,0
+		l0.2,2.3l1.9,0.6l4,0.1l6.7-2.5l-2-3.8L375.4,668.4"/>
+	<path id="SC_Sumter" inkscape:label="Sumter, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M376.5,701.2l1,4.2
+		l19.2,8.5l0.9-0.5l7.5-5.1l1.1-3.1l2.8,7l-11.7,9.9l-11.1,7.1l-6.6,10.3l-6.3-6.3l-3.9-25.7L376.5,701.2"/>
+	<path id="SC_Calhoun" inkscape:label="Calhoun, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M348.5,727l3.1,1.9
+		l9.2,3.9l4.7,0.2l7.7,0.1l6.3,6.3l3.3,5.7l-0.7,1.6l-6.1,5.8l-1.9,0.5l-20-8.2l-5.5-5.2l4.8-3l1.6-1.6l-0.8-0.6l-1.6-0.7l-3.1,0.3
+		l-0.9,1.4l-1.5-0.6l-0.1-6.1l0.1-0.9L348.5,727"/>
+	<path id="SC_Colleton" inkscape:label="Colleton, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M369.7,775.1
+		l11,6.5l0.3,0.2l1.9,0.3l4.1-0.9l7.3-0.6h0.2l0.7,0.5l0.3,0.7l1.3,12.7l0.2,3.5l-1.3,6.4l2.2,3.9l3.6,7.9l2.6,3.6l1.1,0.3l0.5-0.1
+		l0.1-0.5l-0.1-0.2l-0.7-0.6l0.7-0.8l3.7-1.6l0.5,0.4l0,0l-4.4,4.5H405l-5-0.1l-3.4,0.8l-23.5-12.1L354,789l15.3-13.5L369.7,775.1"
+		/>
+	<path id="SC_Allendale" inkscape:label="Allendale, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M354,789
+		l-6.7,5.9l-0.7,0.6l-0.8,1.1l-1,2.9l-0.2,2.7l-0.5,3.4l-2.5,4.3l-2.6,1.8l-1.1,0.6l-11.5-20.3l-5-2.7l3.7-5.4l5.6,0.9l5.6,1.1
+		l4.8-0.8l5.3-1.6L354,789"/>
+	<path id="SC_Bamberg" inkscape:label="Bamberg, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M369.3,775.5
+		L354,789l-7.6-5.6l-2.1-2.4l-0.3-1l-1.8-14.8l-0.5-2.6l-0.7-2l7.1,3.1l6.5,2.2l1.9,0.7l6.1,3.2L369.3,775.5"/>
+	<path id="SC_Orangeburg" inkscape:label="Orangeburg, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M348.7,739.7
+		l5.5,5.2l20,8.2l1.9-0.5l6.1-5.8l0.7-1.6l3.1,3.7l1.9,1.8l2.4,1.6l1.9,0.6l0.8,0.1l1.7-0.3l5.9-1.9l-6.1,14.8l-3.9,0.6l-3.1-0.7
+		l-0.7-0.4l-0.7-1.1l-0.2-0.5l0-0.8l-0.1-0.6l-0.3-0.4l-0.3-0.3l-15.3,13.8l-0.4,0.4l-6.7-5.7l-6.1-3.2l-1.9-0.7l-6.5-2.2l-7.1-3.1
+		l-9.5-2.2l9.2-13.4L348.7,739.7"/>
+	<path id="SC_Barnwell" inkscape:label="Barnwell, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M341,760.6l0.7,2
+		l0.5,2.6L344,780l0.3,1l2.1,2.4l-5.3,1.6l-4.8,0.8l-5.6-1.1l-5.6-0.9l-3.7,5.4l-10.1-4.7l20.2-26.1L341,760.6"/>
+	<path id="SC_Aiken" inkscape:label="Aiken, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M315.5,732.4l0.3,0.4
+		l2.5,2.5l8.1,6.1l1.4,0.2l2.1,0l1.3,0.2l3.7,1l5.8,2.3l-9.2,13.4l-20.2,26.1l-5.6-4.3l-3.9-5.6l-1.4-5.5l-4-4.7l-2.8-2.7l-0.5-0.5
+		l18.4-23.7L315.5,732.4"/>
+	<path id="SC_Lexington" inkscape:label="Lexington, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M325.6,707.2
+		l7,5.9h1.7l2.2,0.2l2.2,1.3l5.4,3.9l1,1l2.2,3.4l0.2,0.4l0.8,3.6l-1.4,0.9l-0.1,0.9l0.1,6.1l1.5,0.6l0.9-1.4l3.1-0.3l1.6,0.7
+		l0.8,0.6l-1.6,1.6l-4.8,3l-7.9,5.3l-5.8-2.3l-3.7-1l-1.3-0.2l-2.1,0l-1.4-0.2l-8.1-6.1l-2.5-2.5l-0.3-0.4l3.7-15.2L325.6,707.2"/>
+	
+		<path id="SC_Richland" inkscape:label="Richland, SC" fill="#C60E3B" stroke="#FFFFFF" stroke-width="1.157" stroke-miterlimit="10" d="
+		M355,697.3l-1.6,6.6l10.2,6.7l5.7-3.2l3.9,25.7l-7.7-0.1l-4.7-0.2l-9.2-3.9l-3.1-1.9l-0.8-3.6l-0.2-0.4l-2.2-3.4l-1-1l-5.4-3.9
+		l-2.2-1.3l-2.2-0.2h-1.7l-7-5.9l0.8-3.2l2,0.1l9.6-0.2l9.2-4.1L355,697.3"/>
+	<path id="SC_Fairfield" inkscape:label="Fairfield, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M348.8,678
+		l0.9,6.2l5.3,13.1l-7.8,2.5l-9.2,4.1l-9.6,0.2l-2-0.1l-9.1-17.4l-0.8-5.6l10.2-1l13.6-1.1L348.8,678"/>
+	<path id="SC_Newberry" inkscape:label="Newberry, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M317.4,686.6
+		l9.1,17.4l-0.8,3.2l-6.5,9.9l-1.8,0.5l-2.6-0.6l-1.7-0.6l-3.9-2.2l-4.5-1.9l-1.5-0.4l-6.7,1.7l-1.6,1.8l0.2,1.1l-5.1-4.2l1.5-3.5
+		l4.7-8l1.6-1.6l3.6-7l2.7-6.3l7.7,4.5l2.1-2.8l0.9-0.4l1.7-0.5l0.8,0L317.4,686.6"/>
+	<path id="SC_Saluda" inkscape:label="Saluda, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M295,716.5l-0.2-1.1
+		l1.6-1.8l6.7-1.7l1.5,0.4l4.5,1.9l3.9,2.2l1.7,0.6l2.6,0.6l1.8-0.5l-3.7,15.2l-4.1,5.2l-11.5-2.1l-1.5-0.9l-0.8-0.7l-2.3-5.8
+		l-2.2,0.2l-4.3,1.7L295,716.5"/>
+	<path id="SC_Edgefield" inkscape:label="Edgefield, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M288.7,730.2
+		l4.3-1.7l2.2-0.2l2.3,5.8l0.8,0.7l1.5,0.9l11.5,2.1L293,761.3l-1-0.9l-5.9-2.5l-5.2-9.1l-0.6-15l6.2-2.7L288.7,730.2"/>
+	<path id="SC_McCormick" inkscape:label="McCormick, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M286.5,731.1
+		l-6.2,2.7l0.6,15l5.2,9.1l-6.9-6.1l-2.4-4.5l-3.7-3.1l-1.6-1.1l-5.2-2.6l-0.3,0l-0.8,0.3l-0.4-0.1l-9-5.3l-2.5-3.8l3.3-4.9l2.5-0.8
+		l2.2,1l0.7,0l3.8-0.4l2.9-0.9l2.9,5.8l4.6-1.2l3.7-0.6l6.3-0.7L286.5,731.1"/>
+	<path id="SC_Greenwood" inkscape:label="Greenwood, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M269.9,700
+		l1.1,1.1l5.1,3.7l8.7,4.5l5.1,2.9l5.1,4.2l-6.3,13.7l-2.2,1l-0.2-2.2l-6.3,0.7l-3.7,0.6l-4.6,1.2l-2.9-5.8l1.9-2.2l1.1-1.5l0.8-2.1
+		l0.3-2.1l-0.3-2.1l-0.8-2l-1.3-1.7l-0.9-0.8l-1.2-0.8l-0.8-0.3l-2.3-4.5L269.9,700"/>
+	<path id="SC_Laurens" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M291,683.2l13,2.5l-2.7,6.3l-3.6,7l-1.6,1.6
+		l-4.7,8l-1.5,3.5l-5.1-2.9l-8.7-4.5l-5.1-3.7l-1.1-1.1l-4.8-4.8l4-4.4l-0.7-3.7l1.8-8.4l1.5-6.7L291,683.2"/>
+	<path id="SC_Abbeville" inkscape:label="Abbeville, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M265.2,695.2
+		l4.8,4.8l-4.5,5.6l2.3,4.5l0.8,0.3l1.2,0.8l0.9,0.8l1.3,1.7l0.8,2l0.3,2.1l-0.3,2.1l-0.8,2.1l-1.1,1.5l-1.9,2.2l-2.9,0.9l-3.8,0.4
+		l-0.7,0l-2.2-1l-2.5,0.8l-3.3,4.9l-10.5-13L265.2,695.2"/>
+	<path id="SC_Anderson" inkscape:label="Anderson, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M232.9,689.5
+		l18.7-17.1l2.7,10.8l0.2,0.6l1,2.3l2.6,4.3l7.1,4.7l-22.3,23.4l-2.2-5.8l-6.3-8.8l-2-3l0-0.2l-0.6-0.5l-1.2-0.4l-2.1,0.3l-2.4,0.6
+		l-0.1,0l-0.7,0.3L232.9,689.5"/>
+	<path id="SC_Union" inkscape:label="Union, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M295.9,659.1l4.1,0.5
+		l7.9,0.4l1.1,0.3l0.7,0.4l1.9,1.4l-1,1.5l6,17.5l0.8,5.6l-0.1-0.1l-0.8,0l-1.7,0.5l-0.9,0.4l-2.1,2.8l-7.7-4.5l-13-2.5l0.9-7.9
+		l0.6-10.2l1.9-3.6L295.9,659.1"/>
+	<path id="SC_Chesterfield" inkscape:label="Chesterfield, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="
+		M378.2,653.3l23-3.3l9.1,18.8l-12.1,5l-12.7,11.5l-10-16.7l-11.3-13.1L378.2,653.3"/>
+	<path id="SC_Lancaster" inkscape:label="Lancaster, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M341,639.9
+		l4.7,4.8l4.2,4.2l0.4,7.4l0,0.7l1.2,0.2l12.6-1.9l11.3,13.1l-7.7,5.3l2,3.8l-6.7,2.5l-4-0.1l-1.9-0.6L357,677l-0.3,0l-7,7.2
+		l-0.9-6.2l-2.2-6.4l0.1-2.5l0.8-3l-1.3-3.6l-1.3-3.2l-0.6-1L341,639.9"/>
+	<path id="SC_Chester" inkscape:label="Chester, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M344.4,658.3l0.6,1
+		l1.3,3.2l1.3,3.6l-0.8,3l-0.1,2.5l2.2,6.4l-8.4,0.9l-13.6,1.1l-10.2,1l-6-17.5L344.4,658.3"/>
+	<path id="SC_York" inkscape:label="York, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M315.5,637.6l16.7-1.5
+		l8.8,3.9l3.4,18.4l-33.9,5.3l1-1.5l-1.1-2l-1.9-5l-0.9-6.4l0-0.4l3.9-8.1l1.7-2.4L315.5,637.6"/>
+	<path id="SC_Cherokee" inkscape:label="Cherokee, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M289.7,640.3
+		l23.5-2.5l-1.7,2.4l-3.9,8.1l0,0.4l0.9,6.4l1.9,5l1.1,2l-1.9-1.4l-0.7-0.4l-1.1-0.3l-7.9-0.4l-4.1-0.5l-3-1.6l-3.2-6.5l-6.3-10
+		L289.7,640.3"/>
+	<path id="SC_Spartanburg" inkscape:label="Spartanburg, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="
+		M277.9,641.5l5.6-0.6l6.3,10l3.2,6.5l3,1.6l-1.5,2.5l-1.9,3.6l-0.6,10.2l-0.9,7.9L271.8,672l-5.2-4.1l-0.2-0.6l-1.3-11.3l-1.7-13
+		L277.9,641.5"/>
+	<path id="SC_Greenville" inkscape:label="Greenville, SC" fill="#283681" stroke="#FFFFFF" stroke-width="1.1565" d="M255.2,643.4
+		l8.2-0.4l1.7,13l1.3,11.3l0.2,0.6l5.2,4.1l-1.5,6.7l-1.8,8.4l0.7,3.7l-4,4.4l-7.1-4.7l-2.6-4.3l-1-2.3l-0.2-0.6l-2.7-10.8l0.3-0.4
+		l-0.5-2.5l-0.6-1.7l-4.2-9.2l-2.6-3.2l-11.1,1.3l10.4-7.1L255.2,643.4"/>
+</g>
+</svg>
+
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
